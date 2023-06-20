fcc5cdf0a8c5d2918827f15e3ca0eda7829eb078
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-20 18:05:51 +0530

    Add project files.

---
+++ Staticvaraiable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticvaraiable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Static r1 = new Static("Rocky", 50000);
+            r1.CalculateSalary();
+            Console.WriteLine(r1.Printe());
+
+            Static r2 = new Static("Pankaj", 1500000);
+            r2.CalculateSalary();
+            Console.WriteLine(r2.Printe());
+
+            Console.WriteLine(Static.GetCount());
+        }
+             
+    }
+}
---
+++ Staticvaraiable/Static.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Staticvaraiable
+{
+    public class Static
+    {
+        private string name;
+        private int empid;
+        private double ta, da, pf, hra, bs, gs, ts;
+        private static int count;
+
+        public Static(string name, int bs)
+        {
+            count++;
+            this.empid = count;
+            this.name = name;
+            this.bs = bs;
+        }
+        public static int GetCount()
+        {
+            return count;
+        }
+        public void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            ta = bs * 0.20;
+            da = bs * 0.15;
+            pf = bs * 0.12;
+
+            ts = (hra + da + ta);
+
+            gs = (hra + da +ta) - pf;
+        }
+        public string Printe()
+        {
+            return $"name is {name}\n id is {empid}\n basic salary is {bs}\n total salary is {ts}\n gross salary is {gs}";
+            
+        }
+       
+        
+           
+         
+    }
+           
+   
+}
